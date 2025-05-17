53e603bd2b21d17d3997e96836c3712629554d86
Savanna Bergen <savanna.bergen@gmail.com>
2025-05-17 14:08:02 -0500

    added husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
-import './index.css'
-import { Button } from './components/atoms/button';
+import { useState } from "react";
+import "./index.css";
+import { Button } from "./components/atoms/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
 
   return (
     <>
-      <Button>Clickme</Button>
+      <Button onClick={handleClick}>Clickme ({count})</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/atoms/button/Button.tsx
+++ src/components/atoms/button/Button.tsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
-import type { ButtonProps } from './button.types';
+import type { ButtonProps } from "./button.types";
 
-const Button = ({ children }: ButtonProps) => {
-  const [count, setCount] = useState(0);
-
+const Button = ({ children, onClick }: ButtonProps) => {
   return (
     <button
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      onClick={() => setCount(count + 1)}
+      onClick={onClick}
     >
-      {children} ({count})
+      {children}
     </button>
   );
 };
 
 export default Button;
+
